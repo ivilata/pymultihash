fd4f150e7bd5603d8d152dc2dc92e70cdb035f99
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2016-01-15 13:54:36 +0100

    trivial versions of `Func` and `Multihash` classes

--- multihash.py
+++ multihash.py
@@ -3,6 +3,38 @@
 # Initial author: Ivan Vilata-i-Balaguer
 # License: MIT
 
+from collections import namedtuple
+from enum import Enum
+
+
+class Func(Enum):
+    """An enumeration of hash functions supported by multihash.
+
+    The value of each member corresponds to its integer code.
+
+    >>> Func.sha1.value == 0x11
+    True
+    """
+    sha1 = 0x11
+    sha2_256 = 0x12
+    sha2_512 = 0x13
+    sha3 = 0x14
+    blake2b = 0x40
+    blake2s = 0x41
+
+
+class Multihash(namedtuple('Multihash', 'func length digest')):
+    """A named tuple representing multihash function, length and digest.
+
+    >>> mh = Multihash(Func.sha1, 20, b'BINARY_DIGEST')
+    >>> mh == (Func.sha1, 20, b'BINARY_DIGEST')
+    True
+    >>> mh == (mh.func, mh.length, mh.digest)
+    True
+    """
+    __slots__ = ()
+
+
 def _test():
     import doctest
     doctest.testmod()
