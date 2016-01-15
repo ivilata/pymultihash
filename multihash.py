91ada6d4f222670dc40cc73b1ae4b21434dda60d
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2016-01-15 13:36:13 +0100

    initial empty implementation

---
+++ multihash.py
@@ -0,0 +1,11 @@
+# pymultihash: Python implementation of the multihash specification
+#
+# Initial author: Ivan Vilata-i-Balaguer
+# License: MIT
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+if __name__ == '__main__':
+    _test()
