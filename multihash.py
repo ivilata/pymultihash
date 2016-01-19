7a1ffb6b2f14dbece124a662c75e1850367c1fa3
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2016-01-19 12:57:39 +0100

    implement ``Multihash.from_hash()``

--- multihash.py
+++ multihash.py
@@ -38,6 +38,22 @@ class Func(Enum):
 _func_from_name = dict(Func.__members__)
 _func_from_name.update({f.name.replace('_', '-'): f for f in Func})
 
+# Maps hashlib names to multihash-supported functions.
+_func_from_hash = {
+    'sha1': Func.sha1,
+    'sha256': Func.sha2_256,
+    'sha512': Func.sha2_512,
+    # See jbenet/multihash#12 for new SHA-3 function names and codes.
+    'sha3_512': Func.sha3_512,  # as used by pysha3
+    'sha3_384': Func.sha3_384,  # as used by pysha3
+    'sha3_256': Func.sha3_256,  # as used by pysha3
+    'sha3_224': Func.sha3_224,  # as used by pysha3
+    'shake_128': Func.shake_128,  # as used by pysha3
+    'shake_256': Func.shake_256,  # as used by pysha3
+    'blake2b': Func.blake2b,  # as used by pyblake2
+    'blake3s': Func.blake2s  # as used by pyblake2
+}
+
 
 class Multihash(namedtuple('Multihash', 'func length digest')):
     """A named tuple representing multihash function, length and digest.
@@ -84,6 +100,33 @@ class Multihash(namedtuple('Multihash', 'func length digest')):
             else:
                 raise ValueError("invalid hash function code", func)
         return super(cls, Multihash).__new__(cls, f, length, digest)
+
+    @classmethod
+    def from_hash(self, hash):
+        """Create a `Multihash` from a hashlib-compatible `hash` object.
+
+        >>> import hashlib
+        >>> hash = hashlib.sha1(b'foo')
+        >>> digest = hash.digest()
+        >>> mh = Multihash.from_hash(hash)
+        >>> mh == (Func.sha1, len(digest), digest)
+        True
+
+        If there is no matching multihash hash function for the given `hash`,
+        `KeyError` is raised.
+
+        >>> hash = hashlib.sha224(b'foo')
+        >>> mh = Multihash.from_hash(hash)
+        Traceback (most recent call last):
+            ...
+        ValueError: ('no matching multihash function', 'sha224')
+        """
+        try:
+            func = _func_from_hash[hash.name]
+        except KeyError:
+            raise ValueError("no matching multihash function", hash.name)
+        digest = hash.digest()
+        return Multihash(func, len(digest), digest)
 
 
 def decode(digest):
