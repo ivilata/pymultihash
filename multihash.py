293fb1d696561180cb2a122a7017a31470bb76f6
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2016-01-21 17:57:23 +0100

    begin module-level docstring

--- multihash.py
+++ multihash.py
@@ -3,6 +3,24 @@
 # Initial author: Ivan Vilata-i-Balaguer
 # License: MIT
 
+"""Python implementation of the multihash specification
+
+This is an implementation of the `multihash`_ specification in Python.
+The main component in the module is the `Multihash` class, a named tuple that
+represents a hash function and a digest created with it, with extended
+abilities to work with hashlib-compatible hash functions, verify the integrity
+of data, and encode itself to a byte string in the binary format described in
+the specification.  The `decode()` function can be used for the inverse
+operation, i.e. converting a byte string into a `Multihash` object.
+
+.. functions
+
+.. codecs
+
+.. _multihash: https://github.com/jbenet/multihash
+
+"""
+
 from collections import namedtuple
 from enum import Enum
 from numbers import Integral
