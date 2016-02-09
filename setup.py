f5a0ce29a044478f29e54efe47bcbe3b748ceda2
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2016-02-09 12:21:40 +0100

    bump version to 0.6.0
    Only changes in testing code.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(thisdir, 'README.rst')) as readme:
 
 setup(
     name='pymultihash',
-    version='0.6.0.dev1',
+    version='0.6.0',
 
     description="Python implementation of the multihash specification",
     long_description=long_description,
